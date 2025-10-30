2b013a6a8ab78e39133b5cc1f316c49512586bf9
gasparswidzinski <gasparswidzinski@gmail.com>
2025-10-29 23:35:37 -0300

    se desarrollo el metodo extract_ip

--- utils.py
+++ utils.py
@@ -7,8 +7,12 @@ from datetime import datetime
 def extract_ip(line):
     """ intenta encontrar una ip"""
     
-    pass
-
+    match = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',line)
+    if match:
+        return match.group(0)
+    else:
+        return None
+    
 def extract_user(line):
     """ intenta encontrar un usuario"""
     
