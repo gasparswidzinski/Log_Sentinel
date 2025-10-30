3f26ed4875f114b051c9b8274ec9a98c04f50450
gasparswidzinski <gasparswidzinski@gmail.com>
2025-10-30 00:21:56 -0300

    se desarrollo funcion main() en main.py, para mas modularizacion

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # main.py
+
+from read_logs import read_log_file
+
+def main():
+    
+    log_path = "logs/sample.log"
+    
+    try:
+        read_log_file(log_path)
+    
+    except Exception as e:
+        print(f"Error al leer el archivo de log: {e}")
+
+if __name__ == "__main__":
+    main()
--- read_logs.py
+++ read_logs.py
@@ -23,8 +23,3 @@ def read_log_file(log_path):
     except FileNotFoundError:
         print(f"Error: El archivo {log_path} no se encontro")
     
-if __name__ == "__main__":
-    log_path = "logs/sample.log"
-    read_log_file(log_path)
-    
-    
