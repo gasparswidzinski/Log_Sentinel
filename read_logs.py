68aebc50a9e95981b46474d3f268f11033ba8c67
gasparswidzinski <gasparswidzinski@gmail.com>
2025-10-30 19:57:45 -0300

    se agrego resumen de eventos

--- read_logs.py
+++ read_logs.py
@@ -1,24 +1,30 @@
 # read_logs.py
 
 from utils import parse_line
+from collections import Counter
 
 def read_log_file(log_path):
     """ Lee un archivo de log y muestra el contenido del mismo en la consola"""
+    count = Counter()
     try:
         
         with open(log_path, 'r', encoding="utf-8") as f:
-            count = 0
+            
             for line in f:
                 line = line.strip()
                 if not line:
                      continue
                  
                  
-                count += 1
-                data = parse_line(line)
-                print(f"{data['event']:<16} | {data['user'] or '-':<10} | {data['ip'] or '-':<15} | {data['raw_line']} | {data['time']}")
             
-            print(f"\nTotal de lineas procesadas: {count}")         
+                data = parse_line(line)
+                event = data['event']
+                count[event] += 1
+                print(f"{data['event']:<16} | {data['user'] or '-':<10} | {data['ip'] or '-':<15} | {data['raw_line']} | {data['time']} | {data.get('processed_at', '')}")
+            
+        print("\nResumen de eventos:")  
+        for evt, qtv in count.items():
+            print(f"{evt:<16} : {qtv}")         
         
     except FileNotFoundError:
         print(f"Error: El archivo {log_path} no se encontro")
