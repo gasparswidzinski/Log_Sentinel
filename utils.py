f06c62eead9a54aae0f2b2a9546680cc321f93d2
gasparswidzinski <gasparswidzinski@gmail.com>
2025-10-29 23:32:19 -0300

    se definieron las primeras 4 funciones de utils.py, quedan a desarrollar

---
+++ utils.py
@@ -0,0 +1,25 @@
+# utils.py
+
+import re
+from datetime import datetime
+
+
+def extract_ip(line):
+    """ intenta encontrar una ip"""
+    
+    pass
+
+def extract_user(line):
+    """ intenta encontrar un usuario"""
+    
+    pass
+
+def classify_event(line):
+    """ intenta clasificar el evento del log"""
+    
+    pass
+
+def parse_line(line):
+    """ intenta devolver un diccionario ya armado"""
+    
+    pass
