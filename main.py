bb97de7c829e7220aff9b923eca3f4cc6c5d2021
gasparswidzinski <gasparswidzinski@gmail.com>
2025-11-04 18:54:05 -0300

    se realizaron ajustes en main.py para utilizar rules.json

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # main.py
 
-
+import json
 from core.parser import LogParser
 from core.analyzer import LogAnalyzer
 from core.reporter import LogReporter
 
 def main():
     
+    with open("rules.json",'r', encoding='utf-8') as f:
+        rules = json.load(f)
+    
     log_path = "logs/sample.log"
     parser = LogParser()
-    analyzer = LogAnalyzer(parser)   
+    analyzer = LogAnalyzer(parser, rules = rules)   
     reporter = LogReporter()
     
     
@@ -19,9 +22,13 @@ def main():
         analyzer.summarize(df)
         
         fuera = analyzer.detect_offhour(df)
+        brute = analyzer.detect_bruteforce(df)
         print("\nEventos fuera de horario laboral:")
         reporter.show_offhours(fuera)
-        reporter.save_report(fuera)
+        reporter.show_offhours(brute)
+        reporter.save_report(df, "reports/full_log_report.csv")
+        reporter.save_report(fuera, "alerts/offhours_report.csv")
+        reporter.save_report(brute, "alerts/bruteforce_report.csv")
     except Exception as e:
         print(f"Error durante el procesamiento: {e}")
      
