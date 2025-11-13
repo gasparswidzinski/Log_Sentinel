13d1d2733f30f0894f95c9ab3c6ed9a76531dd2e
gasparswidzinski <gasparswidzinski@gmail.com>
2025-11-12 21:05:03 -0300

    se utilizo docstring para documentar la funcion append_alert

--- core/alert_storage.py
+++ core/alert_storage.py
@@ -14,3 +14,21 @@ def ensure_history_dir():
     
     os.makedirs(os.path.dirname(HISTORY_PATH), exist_ok=True) 
 
+def append_alert(offhours_df, bruteforce_df, rules = None):
+    
+    """recibe los DataFrames de alertas actuales offhours y bruteforce,
+    los normaliza a un formato comun y los agrega al archivo data/alerts_history.csv
+    -offhours_df: DataFrame con las alertas offhours actuales
+    -bruteforce_df: DataFrame con las alertas bruteforce actuales
+    -rules: lista de reglas aplicadas (opcional)
+    """
+    
+    ensure_history_dir()
+    
+    run_ts = datetime.now() #momento en el que se ejecuta el analisis
+    
+    frames = [] #lista de DataFrames ya normalizados
+    
+    
+    
+    
